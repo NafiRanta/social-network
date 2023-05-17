e59ee1b6387479b935fe609493df474ada527d82
Nafi Rantasalmi <nafisah.rantasalmi@gmail.com>
2023-05-17 19:10:38 +0300

    Login.js view done. missing avatar for user to download

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Login from './views/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
         <Login />
-      </header>
-    </div>
   );
 }
 
