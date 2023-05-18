5b20c6576266efb15bed76f14d5b5482aba00229
Nafi Rantasalmi <nafisah.rantasalmi@gmail.com>
2023-05-18 13:56:49 +0300

    add ChatContainerSmall component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import './App.css';
-import Login from './views/Login';
+/* import Login from './views/Login';
 import Topnav from './views/Topnav'
 import CreatePost from './components/CreatePost'
-import PostContainer from './components/PostContainer'
+import PostContainer from './components/PostContainer' */
+import PostContainer from './components/PostContainer' 
+import ChatContainerSmall from './components/ChatContainerSmall'
 
 function App() {
   return (
-        <PostContainer />
+        <ChatContainerSmall />
   );
 }
 
