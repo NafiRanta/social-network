006d2495301deee761905301f1102ff3235afa30
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-28 01:54:14 +0300

    server setup

---
+++ go.mod
@@ -0,0 +1,11 @@
+module socialnetwork
+
+go 1.17
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/gorilla/sessions v1.2.1
+	github.com/mattn/go-sqlite3 v1.14.16
+)
+
+require github.com/gorilla/securecookie v1.1.1 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	a "socialnetwork/authentication"
+	d "socialnetwork/database"
+	u "socialnetwork/utils"
+)
+
+var Database *sql.DB
+
+func init() {
+	fmt.Println("Server init")
+	Database = d.GetDB()
+}
+
+func main() {
+	err := Start()
+	u.CheckErr(err)
+}
+
+func Start() error {
+	router := http.NewServeMux()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "connected")
+	})
+
+	router.HandleFunc("/login", a.LogIn)
+	handler := u.CorsMiddleware(router)
+
+	//fire up the server
+	log.Print("Listening on :8080...")
+	err := http.ListenAndServe(":8080", handler)
+	u.CheckErr(err)
+	return nil
+}
