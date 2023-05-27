61d57fcb96e2f04094f6647fa9b40f39e822e12b
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-28 01:55:34 +0300

    corse middleware

---
+++ utils/cors.go
@@ -0,0 +1,22 @@
+package utils
+
+import "net/http"
+
+// CORS middleware
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
