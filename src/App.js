49f0c3bda45154d13d84a0f7fb67e95e2d78289f
Nafi Rantasalmi <nafisah.rantasalmi@gmail.com>
2023-05-28 18:46:12 +0300

    update /login path to <Login />

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       ) : (
         <Route path="/" element={<Navigate replace to="/login" />} />
       )}
-    <Route path="/login" element={<Home />} />
+    <Route path="/login" element={<Login />} />
   </Routes>
     // <Home />
     // <Profile />
