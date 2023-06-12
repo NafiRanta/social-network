8be2c6f921ccf1e041787d7d17b41a9758acada5
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-06-12 22:05:59 +0300

    invitefriend create group works. can add & remove

--- src/components/Modal/CreateGroupModal.js
+++ src/components/Modal/CreateGroupModal.js
@@ -1,58 +1,87 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./Modal.css";
 import Avatar from "../Avatar/Avatar";
 
 function CreateGroupModal(props) {
   console.log("username create group", props.username);
-    return(
-        <div className="modal fade" id="createGroupModal" tabIndex="-1" aria-labelledby="createModalLabel" aria-hidden="true" data-bs-backdrop="false">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header align-items-center">
-              <h5 className="text-dark text-center w-100 m-0" id="exampleModalLabel">Create Group</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={props.closeModal}></button>
-            </div>
-            <div className="modal-body">
-              <div className="my-1 p-1">
-                <div className="d-flex flex-column">
-                  <div className="d-flex align-items-center">
-                    <div className="p-2">
-                      <Avatar /> 
-                    </div>
-                    <div>
-                      <p className="m-0 fw-bold">{props.username}</p>
-                    </div>             
+  const [selectedNames, setSelectedNames] = useState([]);
+  const [selectedName, setSelectedName] = useState('');
+
+  const names = ['John', 'Jane', 'Bob', 'Alice'];
+
+  const handleNameChange = (event) => {
+    setSelectedName(event.target.value);
+  };
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+    if (selectedName && !selectedNames.includes(selectedName)) {
+      setSelectedNames([...selectedNames, selectedName]);
+      setSelectedName('');
+    }
+  };
+
+  const handleRemoveName = (name) => {
+    const updatedNames = selectedNames.filter((n) => n !== name);
+    setSelectedNames(updatedNames);
+  };
+
+  return (
+    <div className="modal fade" id="createGroupModal" tabIndex="-1" aria-labelledby="createModalLabel" aria-hidden="true" data-bs-backdrop="false">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header align-items-center">
+            <h5 className="text-dark text-center w-100 m-0" id="exampleModalLabel">Create Group</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={props.closeModal}></button>
+          </div>
+          <div className="modal-body">
+            <div className="my-1 p-1">
+              <div className="d-flex flex-column">
+                <div className="d-flex align-items-center">
+                  <div className="p-2">
+                    <Avatar />
                   </div>
                   <div>
-                      <form id="createGroupForm">
-                          <input type="text" className="form-control my-3" id="groupname" placeholder="Group Name" />
-                          <div>
-                            <textarea cols="30" rows="5" className="form-control border-0" placeholder="Group Description"></textarea>
-                          </div>
-                          <select className="form-select form-control my-3" aria-label="Default select example">
-                            <option disabled selected>
-                              Invite Friends (optional)
-                            </option>
-                            <option value="1">
-                              <div className="d-flex align-items-center">
-                                <Avatar />
-                                <p className="m-0">Jacob</p>
-                              </div>
-                            </option>
-                          </select>
-                      </form>
-                    </div>       
+                    <p className="m-0 fw-bold">{props.username}</p>
+                  </div>
+                </div>
+                <div>
+                  <form id="createGroupForm">
+                    <input type="text" className="form-control my-3" id="groupname" placeholder="Group Name" />
+                    <div>
+                      <textarea cols="30" rows="5" className="form-control border-0" placeholder="Group Description"></textarea>
+                    </div>
+                    <div>
+                      {selectedNames.map((name) => (
+                        <div key={name} className="d-flex align-items-center">
+                          <input type="text" className="form-control me-2" value={name} readOnly />
+                          <button onClick={() => handleRemoveName(name)}>Remove</button>
+                        </div>
+                      ))}
+                    </div>
+                    <select value={selectedName} onChange={handleNameChange} className="form-select form-control my-3">
+                      <option disabled value="">
+                        Invite Friends (optional)
+                      </option>
+                      {names.filter((name) => !selectedNames.includes(name)).map((name) => (
+                        <option key={name} value={name}>
+                          {name}
+                        </option>
+                      ))}
+                    </select>
+                    <button onClick={handleAddName}>Add Name</button>
+                  </form>
                 </div>
               </div>
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-primary w-100">Create</button>
-            </div>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary w-100">Create</button>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default CreateGroupModal;
-
