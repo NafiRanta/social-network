47fcb2ed71d4908a17d5aa74b6bc52304c6a3e10
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-06-13 19:09:52 +0300

    fixing Profile profile-cover

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -6,65 +6,62 @@ import AvatarSquare from '../../components/Avatar/AvatarSquare';
 import './Profile.css';
 import ChangeProfilePicModal from '../../components/Modal/ChangeProfilePicModal';
 
+
 function Profile(props) {
   return (
     <div>
       <Topnav />
       <div className="container-fluid">
-        <section>
-          <div className="container py-5 ">
-            <div className="bg-white" id="bg-white">
-              <div className="panel profile-cover p-4">
-                <div className="card">
-                  <div className="card-body p-4">
-                    <div className="d-flex text-black">
-                      <div className="flex-shrink-0">
-                        <img
-                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                          alt="Generic placeholder image"
-                          className="img-fluid"
-                        />
-                        <i
-                          className="fas fa-images fs-5 text-success pointer position-absolute bottom-0 start-0 ms-2"
-                          data-bs-toggle="modal"
-                          data-bs-target="#changeProfilePicModal"
-                        ></i>
-                      </div>
-
-                      <div className="flex-grow-1 ms-3">
-                        <h5 className="mb-1">{props.username}</h5>
-                        <div className="d-flex justify-content-start rounded-3 p-2 mb-2">
-                          <div>
-                            <p className="small text-muted mb-1">Followers</p>
-                            <p className="mb-0">41</p>
-                          </div>
-                          <div className="px-3">
-                            <p className="small text-muted mb-1">Following</p>
-                            <p className="mb-0">976</p>
-                          </div>
+            <section className="profileTopnav">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-8">
+                        <div className="card">
+                        <div className="card-body p-4">
+                            <div className="d-flex text-black">
+                            <div className="flex-shrink-0">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                alt="Generic placeholder image" class="img-fluid"/>
+                                <i
+                                className="fas fa-images fs-5 text-success pointer position-absolute bottom-0 start-0 ms-2"
+                                data-bs-toggle="modal"
+                                data-bs-target="#changeProfilePicModal"
+                                ></i>
+                            </div>
+                            <div className="flex-grow-1 ms-3">
+                                <h5 class="mb-1">{props.username}</h5>
+                                <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
+                               style={{ backgroundColor: '#efefef' }}>
+                                    <div className="px-3">
+                                        <p className="small text-muted mb-1">
+                                        Followers
+                                        </p>
+                                        <p className="mb-0">976</p>
+                                    </div>
+                                    <div>
+                                        <p className="small text-muted mb-1">
+                                        Following
+                                        </p>
+                                        <p className="mb-0">8.5</p>
+                                    </div>
+                                </div>
+                                <div className="d-flex pt-1">
+                                <button type="button" className="btn btn-outline-primary me-1 flex-grow-1">
+                                    Chat
+                                </button>
+                                <button type="button" className="btn btn-primary flex-grow-1">
+                                    Follow
+                                </button>
+                                </div>
+                            </div>
+                            </div>
                         </div>
-                        <div className="d-flex pt-1">
-                          <button
-                            type="button"
-                            className="btn btn-outline-primary me-1 flex-grow-1"
-                          >
-                            Chat
-                          </button>
-                          <button
-                            type="button"
-                            className="btn btn-primary flex-grow-1"
-                          >
-                            Follow
-                          </button>
                         </div>
-                      </div>
                     </div>
-                  </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
         </section>
+        
         <ChangeProfilePicModal username={props.username} />
                 <div className="row justify-content-evenly">
                     <div className="col-12 col-lg-3">
