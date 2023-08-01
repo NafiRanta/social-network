67f36c8a8cbf3815495f0447e58b15fbfef52a6d
Jacobpes <jacob.pesamaa@gmail.com>
2023-08-01 16:26:12 +0300

    cleanup

--- utils/cors.go
+++ utils/cors.go
@@ -6,7 +6,6 @@ import "net/http"
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		//allowedOrigin := "http://localhost:3000"
 		allowedOrigin := "http://localhost:3000"
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
